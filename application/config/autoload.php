cb7f05989c125ce12da2ec9be1b0182e3e13fdf8
milman20 <thegooners303@gmail.com>
2023-05-24 20:46:42 +0700

    progres pertemuan 9

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('from validation' 'session','database');
+$autoload['libraries'] = array('form_validation','session','database');
 
 /*
 | -------------------------------------------------------------------
